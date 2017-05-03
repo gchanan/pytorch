c7919515be2d60f2f88a91f2c2629dbc7b2b06a5
Gregory Chanan <gchanan@fb.com>
2017-05-03 15:50:44 -0700

    Make keepdim work with autograd.

--- torch/autograd/_functions/reduce.py
+++ torch/autograd/_functions/reduce.py
@@ -5,9 +5,10 @@ from ..function import Function
 
 class _DimReduceFunction(Function):
 
-    def __init__(self, dim=None):
+    def __init__(self, dim=None, keepdim=False):
         super(_DimReduceFunction, self).__init__()
         self.dim = dim
+        self.keepdim = keepdim
 
     def forward(self, input):
         self.input_size = input.size()
@@ -15,7 +16,7 @@ class _DimReduceFunction(Function):
         if self.dim is None:
             return input.new((fn(),))
         else:
-            return fn(self.dim)
+            return fn(self.dim, self.keepdim)
 
 
 class Sum(_DimReduceFunction):
@@ -25,6 +26,9 @@ class Sum(_DimReduceFunction):
         if self.dim is None:
             return grad_output.new(self.input_size).fill_(grad_output[0])
         else:
+            if self.keepdim is False:
+                grad_output = grad_output.unsqueeze(self.dim)
+
             repeats = [1 for _ in self.input_size]
             repeats[self.dim] = self.input_size[self.dim]
             return grad_output.repeat(*repeats),
@@ -39,7 +43,7 @@ class Prod(_DimReduceFunction):
             self.save_for_backward(input)
             return input.new((self.result,))
         else:
-            output = input.prod(self.dim)
+            output = input.prod(self.dim, self.keepdim)
             self.save_for_backward(input, output)
             return output
 
@@ -59,10 +63,13 @@ class Prod(_DimReduceFunction):
                 grad_input[zero_idx] = grad_output[0] * input_copy.prod()
                 return grad_input
         else:
+            if self.keepdim is False:
+                grad_output = grad_output.unsqueeze(self.dim)
+
             input, output = self.saved_tensors
             dim = self.dim if self.dim >= 0 else self.dim + input.dim()
             zero_mask = input == 0
-            slice_zero_count = zero_mask.sum(dim)
+            slice_zero_count = zero_mask.sum(dim, True)
             total_zeros = slice_zero_count.sum()
             grad_input = grad_output.mul(output).expand_as(input).div(input)
             if total_zeros == 0:
@@ -101,6 +108,9 @@ class Mean(_DimReduceFunction):
             grad_input_val /= reduce(lambda x, y: x * y, self.input_size, 1)
             return grad_output.new(*self.input_size).fill_(grad_input_val)
         else:
+            if self.keepdim is False:
+                grad_output = grad_output.unsqueeze(self.dim)
+
             repeats = [1 for _ in self.input_size]
             dim_size = self.input_size[self.dim]
             repeats[self.dim] = dim_size
@@ -114,9 +124,10 @@ class _SelectionFunction(Function):
     # kthvalue not only requires us to pass a dim, but also preceed it with k.
     additional_args = tuple()
 
-    def __init__(self, dim=None):
+    def __init__(self, dim=None, keepdim=False):
         super(_SelectionFunction, self).__init__()
         self.dim = dim
+        self.keepdim = keepdim
 
     def forward(self, input):
         fn = getattr(input, type(self).__name__.lower())
@@ -130,7 +141,7 @@ class _SelectionFunction(Function):
                 dim = input.dim() - 1
             else:
                 dim = self.dim
-            args = (dim,)
+            args = (dim, self.keepdim)
             if self.additional_args:
                 args = self.additional_args + args
             output, indices = fn(*args)
@@ -147,7 +158,13 @@ class _SelectionFunction(Function):
                 dim = input.dim() - 1
             else:
                 dim = self.dim
+
             indices, = self.saved_tensors
+            if self.keepdim is False:
+                grad_output = grad_output.unsqueeze(dim)
+                grad_indices = grad_indices.unsqueeze(dim)
+                indices = indices.unsqueeze(dim)
+
             grad_input.scatter_(dim, indices, grad_output)
         return grad_input
 
@@ -171,17 +188,18 @@ class Median(_SelectionFunction):
 class Kthvalue(_SelectionFunction):
     has_all_reduce = False
 
-    def __init__(self, k, dim=None):
-        super(Kthvalue, self).__init__(dim)
+    def __init__(self, k, dim=None, keepdim=False):
+        super(Kthvalue, self).__init__(dim, keepdim)
         self.additional_args = (k,)
 
 
 class Norm(Function):
 
-    def __init__(self, norm_type=2, dim=None):
+    def __init__(self, norm_type=2, dim=None, keepdim=False):
         super(Norm, self).__init__()
         self.norm_type = norm_type
         self.dim = dim
+        self.keepdim = keepdim
 
     def forward(self, input):
         if self.dim is None:
@@ -189,7 +207,7 @@ class Norm(Function):
             self.save_for_backward(input)
             return input.new((self.norm,))
         else:
-            output = input.norm(self.norm_type, self.dim)
+            output = input.norm(self.norm_type, self.dim, self.keepdim)
             self.save_for_backward(input, output)
             return output
 
@@ -203,14 +221,17 @@ class Norm(Function):
                 scale = grad_output[0] / self.norm ** (self.norm_type - 1)
                 return input.mul(pow).mul(scale)
         else:
+            if self.keepdim is False:
+                grad_output = grad_output.unsqueeze(self.dim)
+
             input, output = self.saved_tensors
             big_grad_output = grad_output.expand_as(input)
             if self.norm_type == 2:
-                big_output = output.expand_as(input)
+                big_output = output.unsqueeze(self.dim).expand_as(input)
                 return input.mul(big_grad_output).div(big_output)
             else:
                 pow = input.abs().pow(self.norm_type - 2)
-                big_output = output.pow(self.norm_type - 1).expand_as(input)
+                big_output = output.unsqueeze(self.dim).pow(self.norm_type - 1).expand_as(input)
                 return input.mul(pow).mul(big_grad_output).div(big_output)
 
 
