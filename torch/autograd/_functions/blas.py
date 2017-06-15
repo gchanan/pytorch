Fix addr broadcasting with autograd.

torch/autograd/_functions/blas.py
@@ -1,7 +1,7 @@
 import torch
 
 from ..function import Function, InplaceFunction
-
+from .utils import maybe_view, maybe_unexpand, maybe_unexpand_or_view
 
 # TODO: no need to save all args if the grad w.r.t. some of them is not needed
 def _get_output(ctx, arg, inplace=False):
@@ -159,6 +159,7 @@ class Addr(InplaceFunction):
     def forward(ctx, add_matrix, vector1, vector2, alpha=1, beta=1, inplace=False):
         ctx.alpha = alpha
         ctx.beta = beta
+        ctx.add_matrix_size = add_matrix.size()
         ctx.save_for_backward(vector1, vector2)
         output = _get_output(ctx, add_matrix, inplace=inplace)
         return torch.addr(alpha, add_matrix, beta,
@@ -170,7 +171,7 @@ class Addr(InplaceFunction):
         grad_add_matrix = grad_vector1 = grad_vector2 = None
 
         if ctx.needs_input_grad[0]:
-            grad_add_matrix = grad_output
+            grad_add_matrix = maybe_unexpand(grad_output, ctx.add_matrix_size)
             if ctx.alpha != 1:
                 grad_add_matrix = grad_add_matrix.mul(ctx.alpha)
 