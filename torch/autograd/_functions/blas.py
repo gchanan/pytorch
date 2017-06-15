Fix autograd broadcast for addmm, baddmm, others.

torch/autograd/_functions/blas.py
@@ -18,6 +18,7 @@ class Addmm(InplaceFunction):
     def forward(ctx, add_matrix, matrix1, matrix2, alpha=1, beta=1, inplace=False):
         ctx.alpha = alpha
         ctx.beta = beta
+        ctx.add_matrix_size = add_matrix.size()
         ctx.save_for_backward(matrix1, matrix2)
         output = _get_output(ctx, add_matrix, inplace=inplace)
         return torch.addmm(alpha, add_matrix, beta,
@@ -29,7 +30,7 @@ class Addmm(InplaceFunction):
         grad_add_matrix = grad_matrix1 = grad_matrix2 = None
 
         if ctx.needs_input_grad[0]:
-            grad_add_matrix = grad_output
+            grad_add_matrix = maybe_unexpand(grad_output, ctx.add_matrix_size)
             if ctx.alpha != 1:
                 grad_add_matrix = grad_add_matrix.mul(ctx.alpha)
 
@@ -52,6 +53,7 @@ class Addbmm(InplaceFunction):
     def forward(ctx, add_matrix, batch1, batch2, alpha=1, beta=1, inplace=False):
         ctx.alpha = alpha
         ctx.beta = beta
+        ctx.add_matrix_size = add_matrix.size()
         ctx.save_for_backward(batch1, batch2)
         output = _get_output(ctx, add_matrix, inplace=inplace)
         return torch.addbmm(alpha, add_matrix, beta,
@@ -63,7 +65,7 @@ class Addbmm(InplaceFunction):
         grad_add_matrix = grad_batch1 = grad_batch2 = None
 
         if ctx.needs_input_grad[0]:
-            grad_add_matrix = grad_output
+            grad_add_matrix = maybe_unexpand(grad_output, ctx.add_matrix_size)
             if ctx.alpha != 1:
                 grad_add_matrix = grad_add_matrix.mul(ctx.alpha)
 
@@ -91,6 +93,7 @@ class Baddbmm(InplaceFunction):
     def forward(ctx, add_batch, batch1, batch2, alpha=1, beta=1, inplace=False):
         ctx.alpha = alpha
         ctx.beta = beta
+        ctx.add_batch_size = add_batch.size()
         ctx.save_for_backward(batch1, batch2)
         output = _get_output(ctx, add_batch, inplace=inplace)
         return torch.baddbmm(alpha, add_batch, beta,
@@ -102,7 +105,7 @@ class Baddbmm(InplaceFunction):
         grad_add_batch = grad_batch1 = grad_batch2 = None
 
         if ctx.needs_input_grad[0]:
-            grad_add_batch = grad_output
+            grad_add_batch = maybe_unexpand(grad_output, ctx.add_batch_size)
             if ctx.alpha != 1:
                 grad_add_batch = grad_add_batch.mul(ctx.alpha)
 
@@ -125,6 +128,7 @@ class Addmv(InplaceFunction):
     def forward(ctx, add_vector, matrix, vector, alpha=1, beta=1, inplace=False):
         ctx.alpha = alpha
         ctx.beta = beta
+        ctx.add_vector_size = add_vector.size()
         ctx.save_for_backward(matrix, vector)
         output = _get_output(ctx, add_vector, inplace=inplace)
         return torch.addmv(alpha, add_vector, beta,
@@ -136,7 +140,7 @@ class Addmv(InplaceFunction):
         grad_add_vector = grad_matrix = grad_vector = None
 
         if ctx.needs_input_grad[0]:
-            grad_add_vector = grad_output
+            grad_add_vector = maybe_unexpand(grad_output, ctx.add_vector_size)
             if ctx.alpha != 1:
                 grad_add_vector = grad_add_vector.mul(ctx.alpha)
 