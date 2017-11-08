74a3b990c01dc5dafdef06b0ca13dcb5c5e70d31
Gregory Chanan <gchanan@fb.com>
2017-11-08 15:43:39 -0800

    single backwards (but needs all to work on empty tensors).

--- test/test_autograd.py
+++ test/test_autograd.py
@@ -1503,12 +1503,13 @@ class TestAutograd(TestCase):
     def test_cat_empty(self):
         f_args_variable = (Variable(torch.randn(0), requires_grad=True),
                            Variable(torch.randn(S, S), requires_grad=True))
-        torch.cat(f_args_variable)
-        # gradcheck doesn't work on empty input Variables
-        #f_args_tensor = deepcopy(unpack_variables(f_args_variable))
-        #run_functional_checks(self, "test_cat_empty", "cat",
-        #                      lambda a, b : torch.cat((a, b)),
-        #                      True, f_args_variable, f_args_tensor)
+        # gradgradcheck doesn't work because gradcheck doesn't work on empty output Variables
+        f_args_tensor = deepcopy(unpack_variables(f_args_variable))
+        run_functional_checks(self, "test_cat_empty", "cat",
+                              lambda a, b: torch.cat((a, b)),
+                              False, f_args_variable, f_args_tensor)
+        self.assertTrue(gradcheck(lambda a, b: torch.cat((a, b)), f_args_variable, eps=1e-6, atol=PRECISION))
+
 
     def test_variable_traverse(self):
         def get_out_and_unrefed_cycle():
