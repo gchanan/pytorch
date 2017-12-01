e736b60c628f466bc71272fbd108573ba6f3b1fa
Gregory Chanan <gchanan@fb.com>
2017-12-01 09:48:18 -0800

    Moved to templatized function.

--- aten/src/ATen/native/NativeFunctions-inl.h
+++ aten/src/ATen/native/NativeFunctions-inl.h
@@ -1,4 +1,5 @@
 #include <type_traits>
+#include "ATen/ApplyUtils.h"
 
 namespace at {
 namespace native {
@@ -9,14 +10,13 @@ template <typename scalartype> bool is_signed(const Tensor& self) {
 
 template <typename scalartype>
 bool allclose2(const Tensor& self, const Tensor& other, double rtol, double atol) {
-  // FixMe: something different for each backend?
-  int equal = 1;
-  ATH_TENSOR_APPLY2(scalartype, scalartype, self, other,
-                    if (equal && *self_data != *other_data) {
-                    equal = 0;
-                    TH_TENSOR_APPLY_hasFinished = 1; break;
-                    });
-  return equal == 1;
+    int equal = 1;
+    ATH_TENSOR_APPLY2(scalartype, scalartype, self, other,
+                      if (equal && *self_data != *other_data) {
+                      equal = 0;
+                      TH_TENSOR_APPLY_hasFinished = 1; break;
+                      });
+    return equal == 1;
 }
 
 }
