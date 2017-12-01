9b6e6c1cb43d22e495d1f3ae533ccb8d1cc5e574
Gregory Chanan <gchanan@fb.com>
2017-12-01 09:38:39 -0800

    Something, doesn't seem to work with templates.

--- aten/src/ATen/native/NativeFunctions-inl.h
+++ aten/src/ATen/native/NativeFunctions-inl.h
@@ -7,5 +7,17 @@ template <typename scalartype> bool is_signed(const Tensor& self) {
   return std::is_signed<scalartype>::value;
 }
 
+template <typename scalartype>
+bool allclose2(const Tensor& self, const Tensor& other, double rtol, double atol) {
+  // FixMe: something different for each backend?
+  int equal = 1;
+  ATH_TENSOR_APPLY2(scalartype, scalartype, self, other,
+                    if (equal && *self_data != *other_data) {
+                    equal = 0;
+                    TH_TENSOR_APPLY_hasFinished = 1; break;
+                    });
+  return equal == 1;
+}
+
 }
 }
