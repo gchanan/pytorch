fb0a8fbc21f057a61d94ebedbc9cc5a5ef950a37
Gregory Chanan <gchanan@fb.com>
2017-12-12 14:59:52 -0800

    Fix gamma grad.

--- aten/src/ATen/native/NativeFunctions.cpp
+++ aten/src/ATen/native/NativeFunctions.cpp
@@ -582,7 +582,7 @@ Tensor RoiPooling2d_backward_cpu(
 template <typename ScalarType>
 static inline ScalarType digamma_one(ScalarType x) {
   const ScalarType eps = x * 1e-2;
-  return (::lgamma(x + eps) - ::lgamma(x - eps)) / (eps + eps);
+  return (std::lgamma(x + eps) - std::lgamma(x - eps)) / (eps + eps);
 }
 
 /** Computes the reparameterized gradient -(d/dalpha cdf(x;alpha)) / pdf(x;alpha)
@@ -595,18 +595,18 @@ static inline ScalarType standard_gamma_grad_one(ScalarType x, ScalarType alpha)
     const ScalarType a0 = 1 / alpha;
     const ScalarType a1 = 1 / (alpha + 1);
     const ScalarType a2 = 1 / (alpha + 2);
-    const ScalarType pow_x_alpha = ::pow(x, alpha);
-    const ScalarType gamma_pdf = ::pow(x, alpha - 1) * ::exp(-x);
+    const ScalarType pow_x_alpha = std::pow(x, alpha);
+    const ScalarType gamma_pdf = std::pow(x, alpha - 1) * std::exp(-x);
     const ScalarType gamma_cdf = pow_x_alpha * (a0 - x*a1 + 0.5f*x*x*a2);
-    const ScalarType gamma_cdf_alpha = (::log(x) - digamma_one(alpha)) * gamma_cdf
+    const ScalarType gamma_cdf_alpha = (std::log(x) - digamma_one(alpha)) * gamma_cdf
         - pow_x_alpha * (a0*a0 - x*a1*a1 + 0.5f*x*x*a2*a2);
     const ScalarType result = -gamma_cdf_alpha / gamma_pdf;
-    return ::isnan(result) ? 0 : result;
+    return std::isnan(result) ? 0 : result;
   }
 
   // Use an asymptotic approximation for large alpha.
   if (alpha > 50.0f) {
-    return (x / alpha);
+    return std::sqrt(x / alpha);
   }
 
   // Use a bivariate rational approximation to the reparameterized gradient.
@@ -626,7 +626,7 @@ static inline ScalarType standard_gamma_grad_one(ScalarType x, ScalarType alpha)
   }
   const ScalarType p = coef_v[0] + v * (coef_v[1] + v * (coef_v[2] + v * coef_v[3]));
   const ScalarType q = coef_v[4] + v * (coef_v[5] + v * (coef_v[6] + v * coef_v[7]));
-  return ::exp(p / q);
+  return std::exp(p / q);
 }
 
 template <typename ScalarType>
