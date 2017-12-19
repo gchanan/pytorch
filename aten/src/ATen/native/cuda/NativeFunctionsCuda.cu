f59c2aeb9171ae5869ab714507d089993c9fad2e
Gregory Chanan <gchanan@fb.com>
2017-12-19 14:48:59 -0800

    GPU stuff.

--- aten/src/ATen/native/cuda/NativeFunctionsCuda.cu
+++ aten/src/ATen/native/cuda/NativeFunctionsCuda.cu
@@ -1,8 +1,34 @@
 #include "ATen/NativeFunctions.h"
+#include "ATen/Dispatch.h"
+#include "ATen/ExpandUtils.h"
+#include "CUDAApplyUtils.cuh"
 #include <cfloat>
 
 namespace at {
 namespace native {
+
+template <typename scalar>
+struct WhereOpCUDA {
+  static void apply(Tensor& ret, const Tensor& condition, const Tensor& self, const Tensor& other) {
+    CUDA_tensor_apply4<scalar, uint8_t, scalar, scalar>(ret, condition, self, other,
+      [] __device__ (scalar& ret_val, const uint8_t& cond_val, const scalar &self_val, const scalar &other_val) {
+        ret_val = cond_val ? self_val : other_val;
+      }
+    );
+  }
+};
+
+Tensor where_cuda(const Tensor& condition, const Tensor& self, const Tensor& other) {
+  if (condition.type().scalarType() != ScalarType::Byte) {
+    runtime_error("Expected condition to have ScalarType Byte, but got ScalarType %s",
+                  toString(condition.type().scalarType()));
+  }
+  Tensor b_condition, b_self, b_other;
+  std::tie(b_condition, b_self, b_other) = expand_outplace(condition, self, other, "where");
+  Tensor ret = b_self.type().tensor(b_self.sizes());
+  at::dispatch_all<WhereOpCUDA>(ret.type(), "where", ret, b_condition, b_self, b_other);
+  return ret;
+}
 
 __host__ __device__ __forceinline__ float fmin(float a, float b) {
   return a > b ? b : a;
