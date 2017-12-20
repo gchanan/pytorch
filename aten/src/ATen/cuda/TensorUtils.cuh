977de0aea2c3fccdac45eafe5de381b8f9fa8a32
Gregory Chanan <gchanan@fb.com>
2017-12-20 15:59:17 -0800

    Fix includes.

--- aten/src/ATen/cuda/TensorUtils.cuh
+++ aten/src/ATen/cuda/TensorUtils.cuh
@@ -9,8 +9,8 @@ namespace tensorutils {
 bool overlappingIndices(const at::Tensor& t);
 bool canUse32BitIndexMath(const at::Tensor &t, ptrdiff_t max_elem=UINT32_MAX);
 
-template <typename ScalarType, typename IndexType>
-TensorInfo<ScalarType, IndexType>
+template <typename scalar, typename IndexType>
+TensorInfo<scalar, IndexType>
 getTensorInfo(const at::Tensor& t) {
   IndexType sz[MAX_TENSORINFO_DIMS];
   IndexType st[MAX_TENSORINFO_DIMS];
@@ -21,8 +21,8 @@ getTensorInfo(const at::Tensor& t) {
     st[i] = t.stride(i);
   }
 
-  return TensorInfo<ScalarType, IndexType>(
-    t.data<ScalarType>(), dims, sz, st);
+  return TensorInfo<scalar, IndexType>(
+    t.data<scalar>(), dims, sz, st);
 }
 
 } // tensorutils
