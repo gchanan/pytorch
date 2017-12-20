d5c96d350e1b847ced0617576498688bf25774ea
Gregory Chanan <gchanan@fb.com>
2017-12-20 15:08:38 -0800

    Remove duplicated code.

--- aten/src/ATen/cuda/TensorInfo.cuh
+++ aten/src/ATen/cuda/TensorInfo.cuh
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "ATen/Tensor.h"
-//#include "TensorInfo.cuh"
 
 namespace at {
   
@@ -271,26 +270,4 @@ struct IndexToOffset<T, IndexType, -1> {
   }
 };
 
-namespace tensorutils {
-
-bool overlappingIndices(const at::Tensor& t);
-bool canUse32BitIndexMath(const at::Tensor &t, ptrdiff_t max_elem=UINT32_MAX);
-
-template <typename ScalarType, typename IndexType>
-TensorInfo<ScalarType, IndexType>
-getTensorInfo(const at::Tensor& t) {
-  IndexType sz[MAX_TENSORINFO_DIMS];
-  IndexType st[MAX_TENSORINFO_DIMS];
-
-  int dims = t.dim();
-  for (int i = 0; i < dims; ++i) {
-    sz[i] = t.size(i);
-    st[i] = t.stride(i);
-  }
-
-  return TensorInfo<ScalarType, IndexType>(
-    t.data<ScalarType>(), dims, sz, st);
-}
-
-} // tensorutils
 } // at
