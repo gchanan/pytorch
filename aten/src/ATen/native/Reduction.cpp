06fa0b933d623fe4a576e4a3edb2109d4b172b92
Gregory Chanan <gchanan@fb.com>
2018-01-11 11:38:14 -0800

    Get rid of std::cerr.

--- aten/src/ATen/native/Reduction.cpp
+++ aten/src/ATen/native/Reduction.cpp
@@ -7,7 +7,6 @@ namespace native {
 
 // FixMe: remove once scalars are fully supported in PyTorch.
 Tensor _scalar_sum(const Tensor& self) {
-  std::cerr << "called _scalar_sum" << std::endl;
   return self.sum();
 }
 
