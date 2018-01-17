94916a0835f8002238e6c7e16c900f55c6e69be2
Gregory Chanan <gchanan@fb.com>
2018-01-17 09:28:04 -0800

    Remove hacks.

--- torch/csrc/autograd/python_variable.h
+++ torch/csrc/autograd/python_variable.h
@@ -24,8 +24,7 @@ struct THPVariable {
 extern PyObject *THPVariableClass;
 
 bool THPVariable_initModule(PyObject *module);
-// FixMe: remove allow_scalar when scalars are fully supported.
-PyObject * THPVariable_Wrap(torch::autograd::Variable var, bool allow_scalar=false);
+PyObject * THPVariable_Wrap(torch::autograd::Variable var);
 PyObject * THPVariable_get_data(THPVariable *self);
 
 inline bool THPVariable_Check(PyObject *obj)
