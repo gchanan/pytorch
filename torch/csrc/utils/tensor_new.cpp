c9ad13c23143c45ea34470199ace0d380cf69228
Gregory Chanan <gchanan@fb.com>
2018-02-20 13:47:12 -0800

    v1

--- torch/csrc/utils/tensor_new.cpp
+++ torch/csrc/utils/tensor_new.cpp
@@ -19,6 +19,22 @@ using namespace at;
 
 namespace torch { namespace utils {
 
+static void lazy_init_cuda() {
+  static std::once_flag once;
+  std::call_once(once, []() {
+    auto module = THPObjectPtr(PyImport_ImportModule("torch.cuda"));
+    if (!module) throw python_error();
+    auto res = THPObjectPtr(PyObject_CallMethod(module.get(), "_lazy_init", ""));
+    if (!res) throw python_error();
+  });
+}
+
+static void maybe_initialize_cuda(const at::Type &type) {
+  if (type.is_cuda()) {
+    lazy_init_cuda();
+  }
+}
+
 static Tensor new_with_sizes(const Type& type, int device, IntList sizes) {
   AutoNoGIL no_gil;
   AutoGPU auto_gpu(device);
@@ -42,6 +58,7 @@ static Tensor new_with_tensor_copy(const Type& type, Tensor other) {
   if (other.type() != type) {
     throw TypeError("expected %s (got %s)", type.toString(), other.type().toString());
   }
+  maybe_initialize_cuda(type);
   return type.copy(other);
 }
 
@@ -98,11 +115,9 @@ static Tensor new_from_data(ScalarType scalarType, PyObject* data) {
 #endif
 
   auto sizes = compute_sizes(data);
-  // TODO: we should pass tensor.sizes() rather than sizes, but this doesn't works
-  // if scalars are disabled because the size changes without WITH_SCALARS.
   auto tensor = autograd::make_variable(CPU(scalarType).tensor(sizes), /*requires_grad=*/false);
   recursive_store(
-      (char*)tensor.data_ptr(), sizes, tensor.strides(), 0,
+      (char*)tensor.data_ptr(), tensor.sizes(), tensor.strides(), 0,
       scalarType, tensor.type().elementSizeInBytes(), data);
   return tensor;
 }
@@ -110,6 +125,7 @@ static Tensor new_from_data(ScalarType scalarType, PyObject* data) {
 Tensor new_from_data(const Type & type, int device, PyObject *data) {
   auto tensor = new_from_data(type.scalarType(), data);
   if (tensor.type() != type) {
+    maybe_initialize_cuda(type);
     AutoNoGIL no_gil;
     AutoGPU auto_gpu(device);
     tensor = tensor.toType(type);
@@ -204,16 +220,16 @@ static Tensor set_requires_grad(Tensor self, bool requires_grad) {
 
 Tensor new_tensor(const Type& type, PyObject* args, PyObject* kwargs) {
   static PythonArgParser parser({
-    "new(Tensor other, *, bool requires_grad=False)",
-    "new(PyObject* data, *, int64_t? device=-1, bool requires_grad=False)",
+    "new_tensor(Tensor other, *, Type dtype=None, bool requires_grad=False)",
+    "new_tensor(PyObject* data, *, Type dtype=None, int64_t? device=-1, bool requires_grad=False)",
   });
 
   PyObject* parsed_args[3];
   auto r = parser.parse(args, kwargs, parsed_args);
   if (r.idx == 0) {
-    return set_requires_grad(new_with_tensor_copy(type, r.tensor(0)), r.toBool(1));
+    return set_requires_grad(new_with_tensor_copy(r.typeWithDefault(1, type), r.tensor(0)), r.toBool(2));
   } else if (r.idx == 1) {
-    return set_requires_grad(new_from_data(type, r.toInt64(1), r.pyobject(0)), r.toBool(2));
+    return set_requires_grad(new_from_data(r.typeWithDefault(1, type), r.toInt64(2), r.pyobject(0)), r.toBool(3));
   }
   throw std::runtime_error("new_tensor(): invalid arguments");
 }
