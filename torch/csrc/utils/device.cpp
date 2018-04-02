c7e400c839608024c6c5b142ffede3a82240342e
Gregory Chanan <gchanan@fb.com>
2018-04-02 13:53:40 -0700

    Add device to variables.

---
+++ torch/csrc/utils/device.cpp
@@ -0,0 +1,26 @@
+#include "device.h"
+#include <stdexcept>
+
+namespace torch {
+
+Device::Device(DeviceType device_type, int64_t device_index, bool is_default)
+    : device_type(device_type), device_index(device_index), is_default(is_default) {
+  if (!is_default) {
+    switch (device_type) {
+      case DeviceType::CUDA:
+        if (device_index < 0) {
+          throw std::runtime_error("device index must be positive");
+        }
+        break;
+      case DeviceType::CPU:
+        if (device_index != 0) {
+          throw std::runtime_error("cpu device index must be 0");
+        }
+        break;
+      default:
+        throw std::runtime_error("unexpected DeviceType");
+    }
+  }
+}
+
+}
