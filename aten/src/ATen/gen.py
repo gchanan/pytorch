6216b647caf1b5b985b4c49c0ef573785e4f754f
Gregory Chanan <gchanan@fb.com>
2018-05-21 13:11:50 -0700

    THNN.

--- aten/src/ATen/gen.py
+++ aten/src/ATen/gen.py
@@ -266,7 +266,7 @@ def generate_storage_type_and_tensor(backend, density, scalar_type, declarations
         env['th_headers'] = [
             '#include <TH/TH.h>',
             '#include <TH/THTensor.hpp>',
-            #'#include <THNN/THNN.h>',
+            '#include <THNN/THNN.h>',
             '#undef THNN_',
             '#include <THS/THS.h>',
             '#include <THS/THSTensor.hpp>',
@@ -383,7 +383,7 @@ def generate_outputs():
                     for file in cwrap_files
                     for d in cwrap_parser.parse(file)]
 
-    #declarations += nn_parse.run(nn_files)
+    declarations += nn_parse.run(nn_files)
     declarations += native_parse.run(native_files)
     declarations = preprocess_declarations.run(declarations)
     for fname, env in generators.items():
