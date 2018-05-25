8372f47ad39d5f59555998cc576e2f09848cd3a6
Gregory Chanan <gchanan@fb.com>
2018-05-25 14:43:42 -0700

    done.

--- aten/src/TH/generic/THStorage.hpp
+++ aten/src/TH/generic/THStorage.hpp
@@ -4,13 +4,28 @@
 
 typedef struct THStorage
 {
-    real *data;
+    at::ScalarType scalar_type;
+    void *data_ptr;
     ptrdiff_t size;
     std::atomic<int> refcount;
     char flag;
     THAllocator *allocator;
     void *allocatorContext;
     struct THStorage *view;
+    template <typename T>
+
+    inline T * data() const {
+      if (scalar_type != at::CTypeToScalarType<th::from_type<real>>::to()) {
+        AT_ERROR("Attempt to access Storage having data type ", at::toString(scalar_type),
+                 " as pointer of type ", typeid(T).name());
+      }
+      return unsafeData<T>();
+    }
+
+    template <typename T>
+    inline T * unsafeData() const {
+      return static_cast<T*>(this->data_ptr);
+    }
 } THStorage;
 
 #endif
--- aten/src/THC/generic/THCStorage.hpp
+++ aten/src/THC/generic/THCStorage.hpp
@@ -4,7 +4,8 @@
 
 typedef struct THCStorage
 {
-    real *data;
+    at::ScalarType scalar_type;
+    void *data_ptr;
     ptrdiff_t size;
     std::atomic<int> refcount;
     char flag;
@@ -12,6 +13,20 @@ typedef struct THCStorage
     void *allocatorContext;
     struct THCStorage *view;
     int device;
+
+    template <typename T>
+    inline T * data() const {
+      if (scalar_type != at::CTypeToScalarType<at::cuda::from_type<T>>::to()) {
+        AT_ERROR("Attempt to access Storage having data type ", at::toString(scalar_type),
+                 " as pointer of type ", typeid(T).name());
+      }
+      return unsafeData<T>();
+    }
+
+    template <typename T>
+    inline T * unsafeData() const {
+      return static_cast<T*>(this->data_ptr);
+    }
 } THCStorage;
 
 #endif
