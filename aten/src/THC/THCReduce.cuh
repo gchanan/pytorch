e10ef69c209560c221bc2a4655a582541e3b6421
Gregory Chanan <gchanan@fb.com>
2018-06-01 12:55:49 -0700

    Squeeze1d.

--- aten/src/THC/THCReduce.cuh
+++ aten/src/THC/THCReduce.cuh
@@ -459,7 +459,7 @@ bool THC_reduceDim(THCState* state,
 
 
   if (!keepdim) {
-    TensorUtils<TensorType>::squeeze1d(state, out, out, dim);
+    THCTensor_squeeze1d(state, out, out, dim);
   }
   return true;
 }
