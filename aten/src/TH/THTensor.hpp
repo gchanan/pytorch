a5db02b57b7365a191d704ed92fd30de5c9b1d7f
Gregory Chanan <gchanan@fb.com>
2018-06-07 11:01:05 -0700

    Done.

--- aten/src/TH/THTensor.hpp
+++ aten/src/TH/THTensor.hpp
@@ -8,7 +8,7 @@
 
 #include <atomic>
 
-typedef struct _THTensor
+typedef struct THTensor
 {
     int64_t *size;
     int64_t *stride;
@@ -31,13 +31,11 @@ typedef struct _THTensor
     inline T * unsafe_data() const {
       return storage->unsafe_data<T>() + storageOffset;
     }
-} _THTensor;
+} THTensor;
 
-#include "generic/THTensor.hpp"
-#include "THGenerateAllTypes.h"
-
-#include "generic/THTensor.hpp"
-#include "THGenerateHalfType.h"
+typedef THTensor _THTensor;
 
 #include "generic/THTensorFastGetSet.hpp"
 #include "THGenerateAllTypes.h"
+
+TH_API void THTensor_free(_THTensor *self);
