06a137f8ae351c6ea15be62f76458c9a34436024
Gregory Chanan <gchanan@fb.com>
2019-03-03 22:22:10 -0800

    Named tensor examples.

---
+++ named_tensor_examples.py
@@ -0,0 +1,20 @@
+# Tensor[N, H, W, C]
+x = torch.randn(100, 50, 50, 3)
+
+# Tensor[N, H, W, C]
+x = torch.randn(100, 50, 50, 3)
+conv(x)  # conv expects NCHW format, but the user forgot to transpose
+
+# Pointwise loss on target of size (batch) and output of size (batch, 1) is wrong
+# and happens frequently.
+
+loss = lambda x, y: (x - y)**2
+output = torch.randn(10, 1)
+targets = torch.randn(10)
+>>> loss(output, targets).shape
+torch.Size(10, 10)  # This is wrong
+
+x = torch.randn(100, 50, 50, 3)
+per_example_norm = torch.randn(100)
+# would be nice if we could add per_example_norm and x without unsqeezing 3 times.
+# if x had, say, 50 dimensions, unsqueezing 50 times is not scalable.
