8883da3dc0e2b3c788cf4acff23572eae6907614
Gregory Chanan <gchanan@fb.com>
2019-07-19 08:38:18 -0700

    Remove unused parameter from FORALL macros.

--- c10/core/ScalarType.h
+++ c10/core/ScalarType.h
@@ -22,41 +22,41 @@ namespace c10 {
 
 // NB: Order matters for this macro; it is relied upon in
 // _promoteTypesLookup and the serialization format.
-#define AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_STUBS(_)        \
-  _(uint8_t, Byte, __) /* 0 */                                  \
-  _(int8_t, Char, __) /* 1 */                                   \
-  _(int16_t, Short, __) /* 2 */                                 \
-  _(int, Int, __) /* 3 */                                       \
-  _(int64_t, Long, __) /* 4 */                                  \
-  _(at::Half, Half, __) /* 5 */                                 \
-  _(float, Float, __) /* 6 */                                   \
-  _(double, Double, __) /* 7 */                                 \
-  _(at::ComplexHalf, ComplexHalf, __) /* 8 */                   \
-  _(std::complex<float>, ComplexFloat, __) /* 9 */              \
-  _(std::complex<double>, ComplexDouble, __) /* 10 */           \
-  _(bool, Bool, __) /* 11 */                                    \
-  _(c10::qint8, QInt8, __) /* 12 */                             \
-  _(c10::quint8, QUInt8, __) /* 13 */                           \
-  _(c10::qint32, QInt32, __) /* 14 */                           \
-  _(at::BFloat16, BFloat16, __) /* 15 */
+#define AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_STUBS(_) \
+  _(uint8_t, Byte) /* 0 */                               \
+  _(int8_t, Char) /* 1 */                                \
+  _(int16_t, Short) /* 2 */                              \
+  _(int, Int) /* 3 */                                    \
+  _(int64_t, Long) /* 4 */                               \
+  _(at::Half, Half) /* 5 */                              \
+  _(float, Float) /* 6 */                                \
+  _(double, Double) /* 7 */                              \
+  _(at::ComplexHalf, ComplexHalf) /* 8 */                \
+  _(std::complex<float>, ComplexFloat) /* 9 */           \
+  _(std::complex<double>, ComplexDouble) /* 10 */        \
+  _(bool, Bool) /* 11 */                                 \
+  _(c10::qint8, QInt8) /* 12 */                          \
+  _(c10::quint8, QUInt8) /* 13 */                        \
+  _(c10::qint32, QInt32) /* 14 */                        \
+  _(at::BFloat16, BFloat16) /* 15 */
 
 
 // If you want to support ComplexHalf for real, add ComplexHalf
 // into this macro (and change the name).  But beware: convert()
 // doesn't work for all the conversions you need...
 #define AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_EXCEPT_COMPLEX_HALF(_) \
-  _(uint8_t, Byte, __)                                             \
-  _(int8_t, Char, __)                                              \
-  _(int16_t, Short, __)                                            \
-  _(int, Int, __)                                                  \
-  _(int64_t, Long, __)                                             \
-  _(at::Half, Half, __)                                            \
-  _(float, Float, __)                                              \
-  _(double, Double, __)                                            \
-  _(std::complex<float>, ComplexFloat, __)                         \
-  _(std::complex<double>, ComplexDouble, __)                       \
-  _(bool, Bool, __)                                                \
-  _(at::BFloat16, BFloat16, __)
+  _(uint8_t, Byte)                                                 \
+  _(int8_t, Char)                                                  \
+  _(int16_t, Short)                                                \
+  _(int, Int)                                                      \
+  _(int64_t, Long)                                                 \
+  _(at::Half, Half)                                                \
+  _(float, Float)                                                  \
+  _(double, Double)                                                \
+  _(std::complex<float>, ComplexFloat)                             \
+  _(std::complex<double>, ComplexDouble)                           \
+  _(bool, Bool)                                                    \
+  _(at::BFloat16, BFloat16)
 
 
 #define AT_FORALL_QINT_TYPES(_)  \
@@ -65,7 +65,7 @@ namespace c10 {
   _(c10::qint32, QInt32, i)
 
 enum class ScalarType : int8_t {
-#define DEFINE_ENUM(_1, n, _2) n,
+#define DEFINE_ENUM(_1, n) n,
   AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_STUBS(DEFINE_ENUM)
 #undef DEFINE_ENUM
       Undefined,
@@ -116,40 +116,40 @@ struct ScalarTypeToCPPType<c10::ScalarType::Bool> {
 }
 
 #define AT_FORALL_SCALAR_TYPES(_) \
-  _(uint8_t, Byte, __)            \
-  _(int8_t, Char, __)             \
-  _(int16_t, Short, __)           \
-  _(int, Int, __)                 \
-  _(int64_t, Long, __)            \
-  _(float, Float, __)             \
-  _(double, Double, __)
+  _(uint8_t, Byte)                \
+  _(int8_t, Char)                 \
+  _(int16_t, Short)               \
+  _(int, Int)                     \
+  _(int64_t, Long)                \
+  _(float, Float)                 \
+  _(double, Double)
 
-#define AT_FORALL_SCALAR_TYPES_AND(SCALARTYPE, _)                              \
-  _(uint8_t, Byte, __)                                                         \
-  _(int8_t, Char, __)                                                          \
-  _(int16_t, Short, __)                                                        \
-  _(int, Int, __)                                                              \
-  _(int64_t, Long, __)                                                         \
-  _(at::Half, Half, __)                                                        \
-  _(float, Float, __)                                                          \
-  _(double, Double, __)                                                        \
-  _(decltype(::c10::impl::ScalarTypeToCPPType<SCALARTYPE>::t), SCALARTYPE, __)
+#define AT_FORALL_SCALAR_TYPES_AND(SCALARTYPE, _)                          \
+  _(uint8_t, Byte)                                                         \
+  _(int8_t, Char)                                                          \
+  _(int16_t, Short)                                                        \
+  _(int, Int)                                                              \
+  _(int64_t, Long)                                                         \
+  _(at::Half, Half)                                                        \
+  _(float, Float)                                                          \
+  _(double, Double)                                                        \
+  _(decltype(::c10::impl::ScalarTypeToCPPType<SCALARTYPE>::t), SCALARTYPE)
 
-#define AT_FORALL_SCALAR_TYPES_AND2(SCALARTYPE1, SCALARTYPE2, _)                                  \
-  _(uint8_t, Byte, __)                                                                            \
-  _(int8_t, Char, __)                                                                             \
-  _(int16_t, Short, __)                                                                           \
-  _(int, Int, __)                                                                                 \
-  _(int64_t, Long, __)                                                                            \
-  _(at::Half, Half, __)                                                                           \
-  _(float, Float, __)                                                                             \
-  _(double, Double, __)                                                                           \
-  _(decltype(::c10::impl::ScalarTypeToCPPType<c10::ScalarType::SCALARTYPE1>::t), SCALARTYPE1, __) \
-  _(decltype(::c10::impl::ScalarTypeToCPPType<c10::ScalarType::SCALARTYPE2>::t), SCALARTYPE2, __)
+#define AT_FORALL_SCALAR_TYPES_AND2(SCALARTYPE1, SCALARTYPE2, _)                              \
+  _(uint8_t, Byte)                                                                            \
+  _(int8_t, Char)                                                                             \
+  _(int16_t, Short)                                                                           \
+  _(int, Int)                                                                                 \
+  _(int64_t, Long)                                                                            \
+  _(at::Half, Half)                                                                           \
+  _(float, Float)                                                                             \
+  _(double, Double)                                                                           \
+  _(decltype(::c10::impl::ScalarTypeToCPPType<c10::ScalarType::SCALARTYPE1>::t), SCALARTYPE1) \
+  _(decltype(::c10::impl::ScalarTypeToCPPType<c10::ScalarType::SCALARTYPE2>::t), SCALARTYPE2)
 
 static inline caffe2::TypeMeta scalarTypeToTypeMeta(ScalarType scalar_type) {
-#define DEFINE_CASE(ctype, name, _) \
-  case ScalarType::name:            \
+#define DEFINE_CASE(ctype, name) \
+  case ScalarType::name:         \
     return caffe2::TypeMeta::Make<ctype>();
 
   switch (scalar_type) {
@@ -167,7 +167,7 @@ static inline caffe2::TypeMeta scalarTypeToTypeMeta(ScalarType scalar_type) {
 
 static inline c10::optional<ScalarType> tryTypeMetaToScalarType(
     caffe2::TypeMeta dtype) {
-#define DEFINE_IF(ctype, name, _)                 \
+#define DEFINE_IF(ctype, name)                    \
   if (dtype == caffe2::TypeMeta::Make<ctype>()) { \
     return {ScalarType::name};                    \
   }
@@ -198,15 +198,15 @@ static inline bool operator==(caffe2::TypeMeta m, ScalarType t) {
   return t == m;
 }
 
-#define DEFINE_CONSTANT(_, name, _2) \
+#define DEFINE_CONSTANT(_, name) \
   constexpr ScalarType k##name = ScalarType::name;
 
 AT_FORALL_SCALAR_TYPES_WITH_COMPLEX_AND_STUBS(DEFINE_CONSTANT)
 #undef DEFINE_CONSTANT
 
 static inline const char* toString(ScalarType t) {
-#define DEFINE_CASE(_, name, _2) \
-  case ScalarType::name:         \
+#define DEFINE_CASE(_, name) \
+  case ScalarType::name:     \
     return #name;
 
   switch (t) {
@@ -218,8 +218,8 @@ static inline const char* toString(ScalarType t) {
 }
 
 static inline size_t elementSize(ScalarType t) {
-#define CASE_ELEMENTSIZE_CASE(ctype, name, _2) \
-  case ScalarType::name:                       \
+#define CASE_ELEMENTSIZE_CASE(ctype, name) \
+  case ScalarType::name:                   \
     return sizeof(ctype);
 
   switch (t) {
