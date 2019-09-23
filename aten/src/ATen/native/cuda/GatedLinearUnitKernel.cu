5e4c1414c39701498fcee1ee4c62e90026fa6ae8
Gregory Chanan <gchanan@fb.com>
2019-09-23 11:37:18 -0700

    Port GatedLinearUnit forward to TensorIterator.
    This involves:
    1) Porting GatedLinearUnit CPU from ATen to TensorIterator
    2) Porting GatedLinearUnit CUDA from THNN to TensorIterator.

---
+++ aten/src/ATen/native/cuda/GatedLinearUnitKernel.cu
@@ -0,0 +1,23 @@
+//#include <limits>
+#include <ATen/native/GatedLinearUnit.h>
+#include <ATen/native/cuda/Loops.cuh>
+#include <ATen/Context.h>
+#include <ATen/Dispatch.h>
+#include <ATen/native/DispatchStub.h>
+#include <ATen/native/TensorIterator.h>
+#include <ATen/AccumulateType.h>
+
+namespace at { namespace native {
+
+void glu_kernel_cuda(TensorIterator& iter) {
+  AT_DISPATCH_FLOATING_TYPES_AND_HALF(iter.dtype(), "glu_cuda", [&]() {
+    gpu_kernel(iter, []GPU_LAMBDA(scalar_t a, scalar_t b) -> scalar_t {
+      using accscalar_t = acc_type<scalar_t, true>;
+      const accscalar_t sigNum = accscalar_t(1) / (accscalar_t(1) + std::exp(accscalar_t(-a)));
+      return sigNum * b;
+    });
+  });
+}
+
+REGISTER_DISPATCH(glu_stub, &glu_kernel_cuda);
+}}
