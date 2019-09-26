595082a8b050f58d0d2dcb6a6c78fc7ed7fe643d
Gregory Chanan <gchanan@fb.com>
2019-09-26 12:04:06 -0700

    non-training example

---
+++ foobar.py
@@ -0,0 +1,30 @@
+import numpy as np
+import torch
+
+sample_size = 10
+feature_dim = 1
+
+weight = np.array([1]* feature_dim)
+full_input = np.random.normal(loc=0, size=(sample_size,feature_dim))
+label = np.matmul(full_input, weight)
+full_input = full_input.astype(np.float32)
+label = label.astype(np.float32)
+
+optimizer = torch.optim.SGD(linear.parameters(), lr=0.5)
+
+inputs = torch.from_numpy(full_input)
+labels = torch.from_numpy(label)
+
+linear = torch.nn.Linear(1, 1, bias=False)
+criterion = torch.nn.MSELoss()
+
+for epoch in range(10):
+    optimizer.zero_grad()
+    # get output from the model, given the inputs
+    outputs = linear(inputs)
+    # get loss for the predicted output
+    loss = criterion(outputs, labels)
+    # get gradients w.r.t to parameters
+    loss.backward()
+    # update parameters
+    optimizer.step()
