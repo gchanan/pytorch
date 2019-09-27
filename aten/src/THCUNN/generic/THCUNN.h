ae2253553b24c8726f4fd579dc7c78732ccc44ea
Gregory Chanan <gchanan@fb.com>
2019-09-27 08:04:42 -0700

    Kill Sqrt.

--- aten/src/THCUNN/generic/THCUNN.h
+++ aten/src/THCUNN/generic/THCUNN.h
@@ -366,19 +366,6 @@ THC_API void THNN_(Square_updateGradInput)(
                   THCTensor *gradOutput,
                   THCTensor *gradInput);
 
-THC_API void THNN_(Sqrt_updateOutput)(
-                  THCState *state,
-                  THCTensor *input,
-                  THCTensor *output,
-                  accreal eps);
-
-THC_API void THNN_(Sqrt_updateGradInput)(
-                  THCState *state,
-                  THCTensor *input,
-                  THCTensor *gradOutput,
-                  THCTensor *gradInput,
-                  THCTensor *output);
-
 THC_API void THNN_(Tanh_updateOutput)(
                   THCState *state,
                   THCTensor *input,
