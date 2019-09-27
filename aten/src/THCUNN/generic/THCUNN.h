6645a071fbf5dd659c210588955018cc3f27ab7c
Gregory Chanan <gchanan@fb.com>
2019-09-27 08:03:02 -0700

    Kill LookupTable.

--- aten/src/THCUNN/generic/THCUNN.h
+++ aten/src/THCUNN/generic/THCUNN.h
@@ -133,25 +133,6 @@ THC_API void THNN_(LogSigmoid_updateGradInput)(
                   THCTensor *gradInput,
                   THCTensor *buffer);
 
-THC_API void THNN_(LookupTable_accGradParameters)(
-                  THCState *state,
-                  THCIndexTensor *input,
-                  THCTensor *gradOutput,
-                  THCTensor *gradWeight,
-                  THCIndexTensor *count,
-                  THCIndexTensor *sorted,       // [OPTIONAL]
-                  THCIndexTensor *indices,      // [OPTIONAL]
-                  bool scaleGradByFreq,
-                  int paddingValue,
-                  accreal scale);
-
-THC_API void THNN_(LookupTable_renorm)(
-                  THCState *state,
-                  THCIndexTensor *idx,
-                  THCTensor *weight,
-                  accreal maxNorm,
-                  accreal normType);
-
 THC_API void THNN_(MSECriterion_updateOutput)(
                   THCState *state,
                   THCTensor *input,
